4c7704f746bbae8abad60d05d1e632cc374d0af4
xiaobo <jacob.xiaobo@gmail.com>
2015-04-09 14:40:59 +0800

    update verify sms API support Gradle import

--- app/src/main/java/com/avos/sms/MyActivity.java
+++ app/src/main/java/com/avos/sms/MyActivity.java
@@ -14,64 +14,66 @@ public class MyActivity extends Activity implements View.OnClickListener {
   /**
    * Called when the activity is first created.
    */
-  View send,verify;
-  EditText phoneEdit,codeEdit;
+  View send, verify;
+  EditText phoneEdit, codeEdit;
   private Activity cxt;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
-    cxt=this;
+    cxt = this;
     findView();
   }
 
   private void findView() {
-    send=findViewById(R.id.sendCode);
-    verify=findViewById(R.id.verify);
-    phoneEdit= (EditText) findViewById(R.id.phoneEdit);
-    codeEdit= (EditText) findViewById(R.id.codeEdit);
+    send = findViewById(R.id.sendCode);
+    verify = findViewById(R.id.verify);
+    phoneEdit = (EditText) findViewById(R.id.phoneEdit);
+    codeEdit = (EditText) findViewById(R.id.codeEdit);
     send.setOnClickListener(this);
     verify.setOnClickListener(this);
   }
 
   @Override
   public void onClick(View v) {
-    int id=v.getId();
-    if(id==R.id.sendCode){
-      final String phone=phoneEdit.getText().toString();
+    int id = v.getId();
+    if (id == R.id.sendCode) {
+      final String phone = phoneEdit.getText().toString();
       sendCode(phone);
-    }else{
-      final String code=codeEdit.getText().toString();
+    } else {
+      final String code = codeEdit.getText().toString();
       verifyCode(code);
     }
   }
 
   private void verifyCode(String code) {
-    AVOSCloud.verifySMSCodeInBackground(code,new AVMobilePhoneVerifyCallback() {
-      @Override
-      public void done(AVException e) {
-        if(e==null){
-          toast(R.string.verifySucceed);
-        }else{
-          e.printStackTrace();
-          toast(R.string.verifyFailed);
-        }
-      }
-    });
+    AVOSCloud.verifySMSCodeInBackground(code, phoneEdit.getText().toString(),
+        new AVMobilePhoneVerifyCallback() {
+          @Override
+          public void done(AVException e) {
+            if (e == null) {
+              toast(R.string.verifySucceed);
+            } else {
+              e.printStackTrace();
+              toast(R.string.verifyFailed);
+            }
+          }
+        });
   }
 
   public void sendCode(final String phone) {
     new AsyncTask<Void, Void, Void>() {
       boolean res;
+
       @Override
       protected Void doInBackground(Void... params) {
         try {
           AVOSCloud.requestSMSCode(phone, "SmsDemo", "注册", 10);
-          res=true;
+          res = true;
         } catch (AVException e) {
           e.printStackTrace();
-          res=false;
+          res = false;
         }
         return null;
       }
@@ -79,9 +81,9 @@ public class MyActivity extends Activity implements View.OnClickListener {
       @Override
       protected void onPostExecute(Void aVoid) {
         super.onPostExecute(aVoid);
-        if(res){
+        if (res) {
           toast(R.string.sendSucceed);
-        }else{
+        } else {
           toast(R.string.sendFailed);
         }
       }
